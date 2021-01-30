bc8dca921dd5a0000ed208e88a625e7ea1e18c3b
Arnaud Kaderi <arnay07@gmail.com>
2021-01-30 13:48:46 +0100

    added preparation commande confirmation

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,23 @@
 import Task from './Task'
+import classnames from 'classnames'
+
 
 const Tasks = ({ tasks, onDelete, onToggle }) => {
-  return (
-    <>
-      {tasks.map((task, index) => (
-        <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
-      ))}
-    </>
-  )
+    return tasks.every((task) => task.reminder === true) ? (
+        <>
+            <div className={classnames('task', 'confirmation')}><h3>Préparation commande terminée</h3></div>
+            {tasks.map((task, index) => (
+                <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle}/>
+            ))}
+        </>
+    ) : (
+        <>
+            {tasks.map((task, index) => (
+                <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle}/>
+            ))}
+        </>
+    );
 }
+
 
 export default Tasks
